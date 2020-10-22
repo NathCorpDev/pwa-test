f597c827e390d7cb56560d9aacb1d0945c821dbf
Aarish Rahman <projects@nathcorp.com>
2020-10-22 14:49:05 +0530

    show/hide button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa';
   displayOverlay: string = 'none';
+  isPWAInstalled: boolean = false;
+  deviceWidth: number = 0;
 
   constructor() { }
+
+  ngOnInit() {
+    this.deviceWidth = screen.width;
+    this.isPWAInstalled = window.matchMedia('(display-mode:standalone)').matches;
+    console.log(this.deviceWidth)
+  }
 
   showOverlay() {
     this.displayOverlay = 'block'
